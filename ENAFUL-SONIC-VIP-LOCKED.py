cc650172c005fc5c0996f4ad7631d375141ca0c9
BOSSENAFUL <enfulemondal9@gmail.com>
2026-05-02 14:22:32 +0530

    Locked VIP Script by Boss Enaful

--- ENAFUL-SONIC-VIP-LOCKED.py
+++ ENAFUL-SONIC-VIP-LOCKED.py
@@ -1,2 +1,1 @@
-python3 -c "import base64; content = open('ENAFUL-SONIC-VIP.py', 'rb').read(); print('import base64, sys; exec(base64.b64decode(' + repr(base64.b64encode(content)) + '))')" > ENAFUL-SONIC-VIP-LOCKED.py
-
+import base64, sys; exec(base64.b64decode(b''))
